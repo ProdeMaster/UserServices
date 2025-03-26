377897b6930421953a4b29147051477d50c40111
Gaston Herrlein <herrleingaston@gmail.com>
2025-03-26 19:34:29 +0100

    feat: traceability is added to requests with Micrometer and Zipkin

--- src/main/java/com/ProdeMaster/UserServices/Controller/AuthController.java
+++ src/main/java/com/ProdeMaster/UserServices/Controller/AuthController.java
@@ -2,6 +2,8 @@ package com.ProdeMaster.UserServices.Controller;
 
 import com.ProdeMaster.UserServices.Model.UserModel;
 import com.ProdeMaster.UserServices.Service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,12 +14,15 @@ import java.util.Map;
 @RequestMapping("/auth")
 public class AuthController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
+
     @Autowired
     private UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Map<String, String> credentials) {
         String token = userService.authenticate(credentials.get("username"), credentials.get("password"));
+        LOGGER.info("Login user: {}", credentials.get("username"));
         if (token != null) {
             return ResponseEntity.ok(Map.of("token", token));
         }
@@ -26,6 +31,7 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@RequestBody UserModel user) {
+        LOGGER.info("Register user: {}", user.getUsername());
         return ResponseEntity.ok(userService.registerUser(user));
     }
 }
--- src/main/java/com/ProdeMaster/UserServices/Controller/UserController.java
+++ src/main/java/com/ProdeMaster/UserServices/Controller/UserController.java
@@ -2,6 +2,8 @@ package com.ProdeMaster.UserServices.Controller;
 
 import com.ProdeMaster.UserServices.Security.JwtUtil;
 import com.ProdeMaster.UserServices.Service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UserController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
     @Autowired
@@ -17,6 +21,7 @@ public class UserController {
 
     @GetMapping("/")
     public ResponseEntity<?> users() {
+        LOGGER.info("All user");
         return ResponseEntity.ok(userService.usersNames());
     }
 
@@ -24,6 +29,7 @@ public class UserController {
     public ResponseEntity<?> user(@RequestHeader("Authorization") String token,  @PathVariable Long id) {
         String tokenVerify = token.substring(7);
         String username = jwtUtil.validateToken(tokenVerify);
+        LOGGER.info("Profile user: {}", username);
         return ResponseEntity.ok(userService.userProfile(username));
     }
 
