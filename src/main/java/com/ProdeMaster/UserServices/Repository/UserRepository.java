49a4191ec55469eac22fdc8307aa44320aaaedb6
Gaston Herrlein <herrleingaston@gmail.com>
2025-04-20 04:10:54 +0200

    feat: the user repository is updated

--- src/main/java/com/ProdeMaster/UserServices/Repository/UserRepository.java
+++ src/main/java/com/ProdeMaster/UserServices/Repository/UserRepository.java
@@ -2,12 +2,21 @@ package com.ProdeMaster.UserServices.Repository;
 
 import com.ProdeMaster.UserServices.Model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
     Optional<UserModel> findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE " +
+            "(:username IS NULL OR u.username LIKE %:username%) AND " +
+            "(:email IS NULL OR u.email LIKE %:email%) AND " +
+            "u.deleted = false")
+    List<UserModel> searchUsers(@Param("username") String username, @Param("email") String email);
 }
 
